2ed5bddcdc66075bb314ac7062d3a6ea85ae4ad8
AS DEVELOPER <asdeveloperszone@gmail.com>
2026-04-05 07:34:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def download():
         'format': 'best[height<=360][ext=mp4]/best[height<=360]/best[ext=mp4]/best',
         'extractor_args': {
             'youtube': {
-                'player_client': ['android_vr'],
+                'player_client': ['mweb'],
             }
         },
     }
